4ccb375e712e44459bb65e4fc8e39966419ae7d9
MD Dubey <dubeyg@whatever.com>
2014-01-10 00:10:15 +0530

    One Class Implemented for different options

---
+++ src/mritunjd/unixtools/cli/InputOptions.java
@@ -0,0 +1,11 @@
+package mritunjd.unixtools.cli;
+
+public class InputOptions {
+    int noOfLines;
+    String fileName;
+
+    InputOptions(int noOfLines, String fileName) {
+        this.noOfLines = noOfLines;
+        this.fileName = fileName;
+    }
+}
