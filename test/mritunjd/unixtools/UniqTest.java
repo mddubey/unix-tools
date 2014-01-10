c2da61667c2d9065a01223e3dc0437cdc4a3e0de
MD Dubey <dubeyg@whatever.com>
2014-01-10 21:48:31 +0530

    Two test cases of UniqTest

--- test/mritunjd/unixtools/UniqTest.java
+++ test/mritunjd/unixtools/UniqTest.java
@@ -5,6 +5,15 @@ import org.junit.Test;
 
 public class UniqTest {
     @Test
+    public void testGetUniqueLines() throws Exception {
+        String input = "Ram\nSam\nSam\nTum\nTum\nlove";
+        String expected = "Ram\nSam\nTum\nlove";
+
+        Uniq uniq = new Uniq(input);
+        Assert.assertEquals(expected,uniq.getUniqLines());
+    }
+
+    @Test
     public void test_gives_unique_lines_from_given_string() throws Exception {
         String input = "I am Here.\n" +
                 "I am Here.\n" +
