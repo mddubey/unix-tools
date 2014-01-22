9b3b0f43af0bd668c4d627b527ddf8913c0c745d
MD Dubey <dubeyg@whatever.com>
2014-01-23 00:36:48 +0530

    user.config added for Head and Tail. But facing a problem you have to specify default lines for head and tail same.

--- src/mritunjd/unixtools/cli/InputOptions.java
+++ src/mritunjd/unixtools/cli/InputOptions.java
@@ -1,5 +1,9 @@
 package mritunjd.unixtools.cli;
 
+import mritunjd.fs.MyFileReader;
+
+import java.io.File;
+
 public class InputOptions {
     private int noOfLines;
     private String fileName;
@@ -9,8 +13,15 @@ public class InputOptions {
         if (args.length > 1) {
             this.noOfLines = Integer.parseInt(args[0].substring(1));
             this.fileName = args[1];
-        } else
+        } else{
             this.fileName = args[0];
+            String user_config = System.getenv("UNIX_TOOLS") +  "/user.config";
+            File config_file = new File(user_config);
+            if(config_file.exists()){
+                String config_text = new MyFileReader().readFile(config_file);
+                this.noOfLines = Integer.parseInt(config_text.split(":")[1]);
+            }
+        }
     }
 
     public int getNoOfLines() {
